12492336bb847d7e3c24faf9071e0031b38c15dd
Lalita <lalilita888@gmail.com>
2022-05-09 16:48:40 -0400

    add map exercises

--- src/com/codePractice/logic1/Logic8.java
+++ src/com/codePractice/logic1/Logic8.java
@@ -11,13 +11,57 @@ public class Logic8 {
      */
     public int withoutDoubles(int die1, int die2, boolean noDoubles) {
         int sum = die1 + die2;
-        if(noDoubles) {
-            if(die1 == die2 && !(die1 == 6)){
+        if (noDoubles) {
+            if (die1 == die2 && !(die1 == 6)) {
                 sum += 1;
-            }else if(die1 == 6 && die2 == 6){
+            } else if (die1 == 6 && die2 == 6) {
                 sum = 7;
             }
         }
         return sum;
     }
+
+    /*
+    Given two int values, return whichever value is larger. However if the two values have the same remainder
+    when divided by 5, then the return the smaller value. However, in all cases, if the two values are
+    the same, return 0. Note: the % "mod" operator computes the remainder, e.g. 7 % 5 is 2.
+    maxMod5(2, 3) → 3
+    maxMod5(6, 2) → 6
+    maxMod5(3, 2) → 3
+     */
+    public int maxMod5(int a, int b) {
+        int result = 0;
+        if (a == b) {
+            result = 0;
+        } else {
+            if (a % 5 == b % 5) {
+                result = Math.min(a, b);
+            } else {
+                result = Math.max(a, b);
+            }
+        }
+        return result;
+    }
+
+    /*
+    You have a red lottery ticket showing ints a, b, and c, each of which is 0, 1, or 2.
+    If they are all the value 2, the result is 10. Otherwise if they are all the same, the result is 5.
+    Otherwise so long as both b and c are different from a, the result is 1. Otherwise the result is 0.
+    redTicket(2, 2, 2) → 10
+    redTicket(2, 2, 1) → 0
+    redTicket(0, 0, 0) → 5
+     */
+    public int redTicket(int a, int b, int c) {
+        int result = 0;
+
+        if (a == 2 && b == 2 && c == 2) {
+            result = 10;
+        } else if (a == b && b == c) {
+            result = 5;
+        } else if (a != b && a != c) {
+            result = 1;
+        }
+        return result;
+    }
+
 }
